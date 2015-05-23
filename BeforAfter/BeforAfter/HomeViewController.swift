4ad6bf60d6abbbffb87e84e6c034d07bec9c7997
Akira Ishii <exilias774@gmail.com>
2015-05-23 16:36:06 +0900

    ホーム画面いい感じになった

--- BeforAfter/BeforAfter/HomeViewController.swift
+++ BeforAfter/BeforAfter/HomeViewController.swift
@@ -8,39 +8,64 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - TableView data source
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MainCell", forIndexPath: indexPath) as! HomeViewTableCell
+        
+        cell.animatedImageURL = NSURL(string: "http://raphaelschaad.com/static/nyan.gif")
+        
+        return cell
     }
-    */
-
+    
+    // MARK: - TableView delegate
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return tableView.frame.size.width + 37 + 60
+    }
+    
+    
 }
 
 
 class HomeViewTableCell: UITableViewCell {
     
+    var animatedImageURL: NSURL? {
+        didSet {
+            if let unwrappedURL = animatedImageURL {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+                    let animatedImage = FLAnimatedImage(GIFData: NSData(contentsOfURL: unwrappedURL))
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.animatedImageView.animatedImage = animatedImage
+                        return
+                    })
+                })
+            }
+        }
+    }
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var animatedImageView: FLAnimatedImageView!
+    
+    
+    override func awakeFromNib() {
+        likeButton.setTitle(ion_ios_heart_outline, forState: .Normal)
+        commentButton.setTitle(ion_ios_chatbubble_outline, forState: .Normal)
+    }
     
     
     @IBAction func didTouchLikeButton(sender: AnyObject) {
