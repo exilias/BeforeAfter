a6ee78626baac547c91db40798121e47f5318b48
Akira Ishii <exilias774@gmail.com>
2015-05-23 15:53:29 +0900

    :beer: Installing ionicons (2.0.1)

---
+++ BeforAfter/Pods/Target Support Files/Pods-BeforAfter-ionicons/Pods-BeforAfter-ionicons-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeforAfter_ionicons : NSObject
+@end
+@implementation PodsDummy_Pods_BeforAfter_ionicons
+@end
--- BeforAfter/Pods/Target Support Files/Pods-BeforAfter/Pods-BeforAfter-environment.h
+++ BeforAfter/Pods/Target Support Files/Pods-BeforAfter/Pods-BeforAfter-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_FLAnimatedImage 0
 #define COCOAPODS_VERSION_PATCH_FLAnimatedImage 8
 
+// ionicons
+#define COCOAPODS_POD_AVAILABLE_ionicons
+#define COCOAPODS_VERSION_MAJOR_ionicons 2
+#define COCOAPODS_VERSION_MINOR_ionicons 0
+#define COCOAPODS_VERSION_PATCH_ionicons 1
+
