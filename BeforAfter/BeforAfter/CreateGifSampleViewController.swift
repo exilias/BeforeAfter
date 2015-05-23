c27a9d44e0bd1a242f31bd4791080c9d9228ecef
Akira Ishii <exilias774@gmail.com>
2015-05-23 10:05:12 +0900

    :heavy_plus_sign: CreateGifSampleViewController追加

---
+++ BeforAfter/BeforAfter/CreateGifSampleViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateGifSampleViewController.swift
+//  BeforAfter
+//
+//  Created by 石井晃 on 2015/05/23.
+//  Copyright (c) 2015年 Mobile Interaction Researcher. All rights reserved.
+//
+
+import UIKit
+
+class CreateGifSampleViewController: UIViewController {
+
+    @IBOutlet weak var previewView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
